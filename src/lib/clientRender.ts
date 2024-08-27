0ed5b3b4aba4f520d2a7f33df1163df9102856e5
Pedro F Marquez <pfernandom@gmail.com>
2024-08-27 16:26:06 -0500

    fix: Handle text nodes in render function

--- src/lib/clientRender.ts
+++ src/lib/clientRender.ts
@@ -3,10 +3,12 @@
 
 import { isFunctionNode, isStaticNode } from "./jsx_parse_utils";
 
+type TagChildren = string | HTMLElement | (HTMLElement | Node)[];
+
 export function tagP<T extends keyof JSX.IntrinsicElements>(
     type: T,
     props: JSX.IntrinsicElements[T],
-    children?: string | HTMLElement | HTMLElement[],
+    children?: TagChildren,
 ): HTMLElement {
     const element = tag(type, children);
     Object.entries(props).forEach(([k, v]) => {
@@ -23,7 +25,7 @@ export function tagP<T extends keyof JSX.IntrinsicElements>(
 
 export function tag<T extends keyof JSX.IntrinsicElements>(
     type: T,
-    children?: string | HTMLElement | HTMLElement[],
+    children?: TagChildren,
 ): HTMLElement {
     const element = document.createElement(type);
     if (Array.isArray(children)) {
@@ -48,34 +50,37 @@ export function tag<T extends keyof JSX.IntrinsicElements>(
  */
 export function clientRender(
     element: string | number | boolean | SXL.StaticElement,
-) {
-    // TODO: Write tests
+): Node | null {
     if (
         typeof element === "string" ||
         typeof element === "number" ||
         typeof element === "boolean"
     ) {
-        return `${element}`;
-    }
-    if (typeof element === "number") {
-        return element;
+        return document.createTextNode(`${element}`);
     }
 
-    const { children, ...props } = element.props;
+    const { children: _, ...props } = element.props;
+    const children = element.children;
     if (typeof element.type === "string") {
         return tagP(
             element.type as keyof SXL.IntrinsicElements,
             props,
-            children?.map((child) => {
-                if (Array.isArray(child)) {
-                    return tag(
-                        "div",
-                        child.map((c) => clientRender(c)),
-                    );
-                } else {
-                    return clientRender(child);
-                }
-            }),
+            children
+                ?.map((child) => {
+                    if (Array.isArray(child)) {
+                        return tag(
+                            "div",
+                            child
+                                .map((c) => clientRender(c))
+                                .map((el) => el as Node)
+                                .filter((el) => !!el),
+                        );
+                    } else {
+                        return clientRender(child);
+                    }
+                })
+                .filter((el) => !!el)
+                .map((el) => el as Node),
         );
     } else if (isFunctionNode(element)) {
         const child = element.type(element.props);
@@ -84,4 +89,5 @@ export function clientRender(
             return clientRender(child);
         }
     }
+    return null;
 }
